c911b70605d15f921725d30dc1d6cb26016597e6
yuqingl <yuqingl@umich.edu>
2020-07-04 08:06:16 -0400

    Modulo update

--- Modulo.py
+++ Modulo.py
@@ -6,10 +6,10 @@ from networkx.algorithms import bipartite, distance_measures, approximation, cyc
 from tabulate import tabulate
 import networkx as nx
 import scipy.linalg as la
-import matplotlib as mpl
+
 from matplotlib import pyplot as plt
 import networkx.linalg.spectrum as spec
-
+import numpy as np
 from pylab import rcParams
 from networkx.drawing.nx_agraph import to_agraph
 import graph_generation
@@ -98,10 +98,20 @@ def shortest_cycle(n: int) -> int:
         return ans
 
 
+from collections import Counter
+
+
+def countDistinct(arr):
+    # counter method gives dictionary of elements in list
+    # with their corresponding frequency.
+    # using keys() method of dictionary data structure
+    # we can count distinct values in array
+    return len(Counter(arr).keys())
+
 class Hexagon:
     neighbor_hexagons: List[Any]
 
-    def __init__(self, p: int, label: int, init_list: list = []) -> object:
+    def __init__(self, p: int, label: int, init_list: list = [],real_list:list = []) -> object:
         """
 
         :type init_list: object
@@ -109,6 +119,7 @@ class Hexagon:
         self.p = p
         self.label = label
         self.fold_angles = init_list
+        self.real_angles = real_list
         self.neighbors = []
         self.neighbor_hexagons = []
         self.generated = False
@@ -120,12 +131,23 @@ class Hexagon:
             self.generated = True
             for i in range(6):
                 if self.fold_angles[i] > 0:
-                    c = self.fold_angles.copy()
+
+                    d = self.real_angles.copy()
                     i_l = (i - 1) % 6
                     i_r = (i + 1) % 6
-                    c[i_l] = (c[i_l] + 2 * c[i]) % self.p
-                    c[i_r] = (c[i_r] + 2 * c[i]) % self.p
-                    c[i] = (-c[i]) % self.p
+
+                    d[i_l] = d[i_l] + 2 * d[i]
+                    d[i_r] = d[i_r] + 2 * d[i]
+                    d[i] = -d[i]
+
+                    c = self.fold_angles.copy()
+
+
+                    c[i_l] = d[i_l] % self.p
+                    c[i_r] = d[i_r] % self.p
+                    c[i] = d[i] % self.p
+
+
                     # update in the vertex_dict
                     k = [key for key, val in vertex_dict.items() if
                          val.fold_angles == c]  # vertex_dict: {vertex_num: hexagon}
@@ -136,10 +158,11 @@ class Hexagon:
                     else:  # create a new hexagon vertex
                         vertex_num = vertex_num + 1
 
-                        h = Hexagon(p, vertex_num, c)
+                        h = Hexagon(p, vertex_num, init_list=c,real_list=d)
                         vertex_dict[vertex_num] = h
                         self.neighbors.append(vertex_num)
                         self.neighbor_hexagons.append(h)
+
         return self.neighbor_hexagons
 
     def hexagon_img(self):
@@ -154,7 +177,9 @@ class Hexagon:
 if __name__ == '__main__':
 
     info_list = []
-    p_list = [11]
+    p_list = [3,4,5,6,7,8,9,10,11]
+
+    desired_list = [129,2,5,7,10,77,20,53,29]
 
     graph_list = []
     for i in p_list:
@@ -164,8 +189,8 @@ if __name__ == '__main__':
         diG = nx.DiGraph()
         G = nx.Graph()
         sim_G = nx.DiGraph()
-
-        start_point = Hexagon(p, vertex_num, [0, 1, 0, 0, 1, 0])
+        start_list = [0, 1, 0, 0, 1, 0]
+        start_point = Hexagon(p, vertex_num, start_list, start_list)
         # start_point.hexagon_img()
         vertex_dict[vertex_num] = start_point
         q = Queue()
@@ -178,34 +203,48 @@ if __name__ == '__main__':
                     q.put(n)
 
         for k in vertex_dict:
+            """
             if k < 35:
                 sim_G.add_node(k)
                 for n in vertex_dict[k].neighbors:
                     if 35 > n and n > k:
                         sim_G.add_edge(k, n)
-            """"  
+            """
             diG.add_node(k)
             G.add_node(k)
             for n in vertex_dict[k].neighbors:
                 assert isinstance(n, int)
+
+
                 diG.add_edge(k, n)
+                G.add_edge(k, n)
+                add_edge(k, n)
+                """
                 if n > k:
                     G.add_edge(k,n)
                     add_edge(k,n)
-            """
-        """"
+`               """
+
+        """
         #generate gradient map
         d = {}
         for k in vertex_dict:
-            if k == 1:
+            if k == 0:
                 d[k] = 0
             else:
                 #find the nearest neighbor
-                for n in vertex_dict[k].neighbors:
-                    if n < k:
-                        d[k] = d[n]+1
-        #print(d)
+                n = min(vertex_dict[k].neighbors)
+                d[k] = d[n]+1
 
+        for v in d:
+            if v in desired_list:
+                print("index")
+                print(v)
+                print("distance")
+                print(d[v])
+
+       
+        
         #H = nx.relabel_nodes(G, vertex_dict)
         low, *_, high = sorted(d.values())
         norm = mpl.colors.Normalize(vmin=low, vmax=high, clip=True)
@@ -228,8 +267,8 @@ if __name__ == '__main__':
         
         #plt.savefig("gradient_modulo_" + str(p) + ".png")  # save as png
         #plt.show()
-        """
-        # a better way?
+        
+        # a better way to draw?
 
         render = to_agraph(sim_G)  # this is using graphviz. Graphviz worked better than matplotlib in this case.
 
@@ -247,20 +286,56 @@ if __name__ == '__main__':
         A.layout('dot')
         A.draw('dot_multi_'+ str(p)+'.png')
         """
-        A = nx.to_numpy_matrix(diG)
+        A = nx.to_numpy_matrix(G)
+        #print(A)
         eigvals, eigvecs = la.eig(A)
         eigvals = eigvals.real
         eigvals.sort()
+        distinct_eigval_num = countDistinct(eigvals)
         first = eigvals[-1]
         second = eigvals[-2]
         #spec.adjacency_spectrum(G)
-        #girth_list = [len(c) for c in cycles.minimum_cycle_basis(G)]
-        n = vertex_num + 1
 
-        girth = shortest_cycle(n)/2
-
-        info = [p, n, bipartite.is_bipartite(G), distance_measures.diameter(G), girth , first, second]
-        info_list.append(info)
         """
+        girth_dict = {}
+        girth_len_dict = {}
+        for c in cycles.minimum_cycle_basis(G):
+            if len(c) == 9:
+                cycle_9_list.append(c)
+            
+            if len(c) not in girth_dict.keys():
+                girth_dict[len(c)] = c
+                girth_len_dict[len(c)] = 1
+            else:
+                girth_len_dict[len(c)] = girth_len_dict[len(c)] + 1
+        
 
+        cycle_9_list = [[129,2,5,7,10,77,20,53,29],[]]
+        n = vertex_num + 1
+        for cycle_9 in cycle_9_list:
+            for v in cycle_9:
+                print("index" + str(v))
+
+                print("fold_angles:")
+                print(vertex_dict[v].fold_angles)
+                print("real_angles")
+                print(vertex_dict[v].real_angles)
+                print("neighbors:")
+                print(vertex_dict[v].neighbors)
+                print("distance:")
+                print(d[v])
+
+
+        """
+        #print(len(vertex_dict))
+
+        #girth = shortest_cycle(n)/2
+        info = [p, distinct_eigval_num, first, second]
+        #info = [p, n, girth_dict, girth_len_dict]
+        #info = [p, n, bipartite.is_bipartite(G), distance_measures.diameter(G), girth , first, second]
+        info_list.append(info)
+
+
+    print(tabulate(info_list,headers = ["p","# of dictinct eigenvalues","first","second"]))
     #print(tabulate(info_list, headers=["p", "vertex number", "is bipartite", "diameter", "girth", "largest eigenval", "2nd largest eigenval"]))
+    #print(tabulate(info_list, headers=["p", "vertex number", "girth list","girth length dict"]))
